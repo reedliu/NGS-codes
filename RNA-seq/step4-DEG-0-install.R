fc90e96c05d84d534e34a64c98fec18e8a6e79e5
reed_liu <jieandze1314@gmail.com>
2018-12-22 11:10:36 +0800

    add multi-install

--- RNA-seq/step4-DEG-0-install.R
+++ RNA-seq/step4-DEG-0-install.R
@@ -1,24 +1,16 @@
 ##############################
 ## install cran
 ##############################
+
+
 if(length(getOption("CRAN"))==0) options(CRAN="https://mirrors.tuna.tsinghua.edu.cn/CRAN/")
 
-if(!require("tidyr")) install.packages("tidyr",update = F,ask = F)
-
-if(!require("dplyr")) install.packages("dplyr",update = F,ask = F)
-
-if(!require("ggplot2")) install.packages("ggplot2",update = F,ask = F)
-
-if(!require("devtools")) install.packages("devtools",update = F,ask = F)
-
-if(!require("pheatmap")) install.packages("pheatmap",update = F,ask = F)
-
-if(!require("ggfortify")) install.packages("ggfortify",update = F,ask = F)
-
-if(!require("stringr")) install.packages("stringr",update = F,ask = F)
-
-if(!require("ggpubr")) install.packages("ggpubr",update = F,ask = F)
-
+for (pkg in c("tidyr","dplyr","ggplot2","devtools",'pheatmap')){
+  if (! require(pkg,character.only=T) ) {
+    install.packages(pkg,ask = F,update = F)
+    require(pkg,character.only=T) 
+    }
+}
 
 ##############################
 ## install bioconductor
@@ -33,15 +25,9 @@ if(length(getOption("BioC_mirror"))==0) options(BioC_mirror="https://mirrors.ust
 
 
 # use BiocManager to install
-if(!require("limma")) BiocManager::install("limma",update = F,ask = F)
-
-if(!require("GO.db")) BiocManager::install("GO.db",update = F,ask = F)
-
-if(!require("clusterProfiler")) BiocManager::install("clusterProfiler",update = F,ask = F)
-
-if(!require("DESeq2")) BiocManager::install("DESeq2",update = F,ask = F)
-
-if(!require("edgeR")) BiocManager::install("edgeR",update = F,ask = F)
-
-# load multiple packages at one time
-lapply(c('limma','DESeq2','edgeR'),function(x) suppressMessages(library(x,character.only = T)))
+for (pkg in c("limma","GO.db","clusterProfiler","DESeq2",'edgeR')){
+  if (! require(pkg,character.only=T) ) {
+    BiocManager::install(pkg,ask = F,update = F)
+    require(pkg,character.only=T) 
+    }
+}
